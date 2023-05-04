38bb6ce843dda0ea79a624fc0b4cd6616ea6aa9b
Enzo Weder <enzowederma@gmail.com>
2023-05-04 20:07:13 -0300

    ✨ feat: add newInput to simpleInterest

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SimpleInterest from './components/simpleInterest/simpleInterest';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="App-body">
+        <SimpleInterest />
+      </body>
+    </div>
+  );
+}
+
+export default App;
--- src/components/simpleInterest/simpleInterest.js
+++ src/components/simpleInterest/simpleInterest.js
@@ -1,11 +1,26 @@
 import './simpleInterest.css';
 
-const simpleInterest = () => {
+const newInput = (id) => {
+  id = String(id)
+  const name = id[0].toUpperCase() + id.substring(1);
+
+  return(
+    <div>
+      <label for={id}>{name}</label>
+      <br/>
+      <input type='text' id={id}></input>
+    </div>
+  )
+}
+
+const SimpleInterest = () => {
+  const values = ["capital", "montante", "juros", "taxa", "tempo"];
+
   return (
-    <div className="main-container">
-        
+    <div>
+      {newInput(values[0])}
     </div>
   );
 }
 
-export default simpleInterest;
+export default SimpleInterest;
