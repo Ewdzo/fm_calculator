e77c0bd934d8b8e47c4a3853887322e59a77fe4d
Enzo Weder <enzowederma@gmail.com>
2023-05-07 11:55:39 -0300

    ✨ feat: add calculateInputs to Utils.js
    Add calculateInputs function to calculate missing values in simpleInterest

--- src/Utils.js
+++ src/Utils.js
@@ -12,3 +12,20 @@ export const newInput = (id, type) => {
       </div>
     )
 }
+
+export const calculateInputs = (type) => {
+  const inputs = document.querySelectorAll(`[id^="${type}"]`);
+  const values = Array.from(inputs, inputs => inputs.value);
+
+  if(type == "simples"){
+    if(!values[0]) inputs[0].value = Number(values[2] / (values[3] * values[4])).toFixed(2); 
+  
+    if(!values[2]) inputs[2].value = Number(values[0] * values[3] * values[4]).toFixed(2); 
+    
+    if(!values[3]) inputs[3].value = Number(values[2] / (values[0] * values[4])).toFixed(2); 
+    
+    if(!values[4]) inputs[4].value = Number(values[2] / (values[0] * values[3])).toFixed(2); 
+    
+    if(!values[1]) inputs[1].value = (Number(inputs[0].value) + Number(inputs[2].value)).toFixed(2);
+  }
+}
