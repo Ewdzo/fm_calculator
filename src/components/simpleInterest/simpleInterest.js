5ddc5d3e9b924b0c49eefc882e74b9d86dadc629
Enzo Weder <enzowederma@gmail.com>
2023-05-07 11:56:04 -0300

    ✨ feat: simpleInterest calculator
    SimpleInterest now returns a functional calculator

--- src/components/simpleInterest/simpleInterest.js
+++ src/components/simpleInterest/simpleInterest.js
@@ -1,14 +1,16 @@
-import { newInput } from '../../Utils';
+import { calculateInputs, newInput } from '../../Utils';
 import './simpleInterest.css';
 
 const SimpleInterest = () => {
   const inputValues = ["capital", "montante", "juros", "taxa", "tempo"];
 
   return (
-    <div>
+    <div className='simpleInterest-container'>
       <h1>Juro Simples</h1>
-      {inputValues.map((value) => newInput(value, "simples"))}
-      <button>Calcular</button>
+      <div id='simpleInterest-input-container'>
+        {inputValues.map((value) => newInput(value, "simples"))}
+      </div>
+      <button onClick={() => calculateInputs("simples")}>Calcular</button>
     </div>
   );
 }
