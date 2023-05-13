1b6aea3ed811932d3325af6e0438dbcd418c25a9
Enzo Weder <enzowederma@gmail.com>
2023-05-13 19:58:07 -0300

    ✨ feat: change taxCalculator inputs

--- src/components/taxCalculator/TaxCalculator.js
+++ src/components/taxCalculator/TaxCalculator.js
@@ -2,7 +2,7 @@ import { calculateInterest, newInput } from '../../Utils';
 import './TaxCalculator.css';
 
 const TaxCalculator = (props) => {
-  const inputValues = ["taxa", "tempo", "capitalizacao"];
+  const inputValues = ["taxaEfetiva", "tempo", "taxa"];
 
   return (
     <div className={props.type +"-container"}>
