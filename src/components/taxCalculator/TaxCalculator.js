bd89ae44e072e98e28f3a01766096fc84771b449
Enzo Weder <enzowederma@gmail.com>
2023-05-20 18:25:42 -0300

    ✨ feat: add input parameter to taxCalculator

--- src/components/taxCalculator/TaxCalculator.js
+++ src/components/taxCalculator/TaxCalculator.js
@@ -1,14 +1,12 @@
 import { calculateTax, newInput } from '../../Utils';
 import './TaxCalculator.css';
 
-const TaxCalculator = (props) => {
-  const inputValues = ["taxaEfetiva", "tempo", "taxa"];
-
+const TaxCalculator = (props) => {  
   return (
     <div className={props.type +"-container"}>
       <h1>{props.name}</h1>
       <div id={props.type +"-input-container"}>
-        {inputValues.map((value) => newInput(value, props.type))}
+        {props.inputs.map((value) => newInput(value, props.type))}
       </div>
       <button onClick={() => calculateTax(props.type)}>Calcular</button>
     </div>
